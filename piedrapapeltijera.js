c7c1e998f7df614b6893e127dd2897e6bb97f5c3
Olga Cobo Lopez <olgacobolopez@gmail.com>
2021-03-15 14:51:48 +0100

    Juego de piedra papel tijera en funcion de expresion js

---
+++ piedrapapeltijera.js
@@ -0,0 +1,23 @@
+//creo las 3 variables
+var pi="piedra";
+var pa="papel";
+var ti="tijera";
+//creo una funcion a la que le paso por parametro
+var resultado=function(opUser,opPC){
+    
+    if (opUser != opPC){
+        //si las opciones de user y pc son distintas:
+        //comprobamos todas las opciones en las que el user puede ganar con ||
+        if ( (opUser===pi && opPC===pa) || (opUser===pa && opPC===pi) || (opUser===ti && opPC===pa) ){
+            console.log("Gana el usuario");
+        }
+        else{//si no es ninguna de las ociones donde gana el user, gana el pc
+            console.log("Gana el PC");
+        }
+    }
+    else{
+        console.log("Empate");
+    }
+}
+
+resultado(pi,pa)
