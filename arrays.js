d6924df4621db80cde5f6df4bd11d66557b4a039
Olga Cobo Lopez <olgacobolopez@gmail.com>
2021-03-15 17:04:52 +0100

    añado while a array

--- arrays.js
+++ arrays.js
@@ -4,6 +4,16 @@ function saludar(estudiante){
     console.log(`Hola,${estudiante}`);
 }
 
-for(var i=0; i<array.length; i++){
-    saludar(array[i]);
+//for clasico
+// for(var i=0; i<array.length; i++){
+//     saludar(array[i]);
+// }
+
+
+//for guay
+//for(var estudiante of array){ saludar(estudiante);}
+
+while(array.length>0){
+    var estudiante=array.shift(); //me va a sacar en cada vuelta el primer elemento del array
+    saludar(estudiante);
 }
